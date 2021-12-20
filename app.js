bea1714af270d504a5f5dffd6f863f04a05bc208
Gilson Meira <gilsonzi@yahoo.com>
2021-12-20 20:33:37 +0100

    Update app.js
    Incremento de uma variável para fácil troca de canais.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let statusURL = "";
 function addLike(url) {  
 		if( statusURL != window.location.href ) {
 			setTimeout( e => {
-			     	let isName = document.querySelector("#text > a").textContent == "Paulo Momento"; 
+				let canal = 'Digite aqui o nome do canal'
+			     	let isName = document.querySelector("#text > a").textContent == canal; 
 				let isLiked = document.querySelector("#top-level-buttons > ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button.style-default-active"); 
 				if( isName && !isLiked){ 
 					document.querySelector("#top-level-buttons > ytd-toggle-button-renderer:nth-child(1)").click(); 
